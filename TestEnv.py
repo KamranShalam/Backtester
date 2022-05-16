360ceacb4f66f687c931246b2b7852b6f182e1af
Kamran Shalam <u1864674@unimail.hud.ac.uk>
2022-05-16 20:19:30 +0100

    init

---
+++ TestEnv.py
@@ -0,0 +1,54 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+
+plt.style.use("dark_background")
+plt.grid(color ="lightgrey", alpha = 0.2)
+currency = "GBPUSD=X"
+ma1 = 20
+ma2 = 70
+
+start = dt.datetime.now() - dt.timedelta(days = 365 * 5)
+end = dt.datetime.now()
+
+data = web.DataReader(currency, 'yahoo', start, end)
+data[f'SMA_{ma1}'] = data['Adj Close'].rolling(window=ma1).mean()
+data[f'SMA_{ma2}'] = data['Adj Close'].rolling(window=ma2).mean()
+
+"""data = data.iloc[ma2:]"""
+
+"""plt.plot(data['Adj Close'], label = "Gold Price",color ="green")
+plt.plot(data[f'SMA_{ma1}'], label = f"SMA_{ma1}",color = "red")
+plt.plot(data[f'SMA_{ma2}'], label = f"SMA_{ma2}",color = "blue")
+plt.legend(loc = "upper left")
+"""
+
+buysignals = []
+sellsignals = []
+trigger = 0
+
+for x in range (len(data)):
+    if data [f'SMA_{ma1}'].iloc[x] > data[f'SMA_{ma2}'].iloc[x] and trigger != 1:
+        buysignals.append(data['Adj Close'].iloc[x])
+        sellsignals.append(float('nan'))
+        trigger = 1
+    elif data [f'SMA_{ma1}'].iloc[x] < data[f'SMA_{ma2}'].iloc[x] and trigger != -1:
+        sellsignals.append(data['Adj Close'].iloc[x])
+        buysignals.append(float('nan'))
+        trigger = -1
+    else:
+        buysignals.append(float('nan'))
+        sellsignals.append(float('nan'))
+
+data['Buy'] = buysignals
+data['Sell'] = sellsignals
+
+print(data)
+
+plt.plot(data['Adj Close'], label = currency + " Price",color ="white")
+plt.plot(data[f'SMA_{ma1}'], label = f"SMA_{ma1}",color = "purple", linestyle= "--")
+plt.plot(data[f'SMA_{ma2}'], label = f"SMA_{ma2}",color = "yellow", linestyle= "--")
+plt.scatter(data.index, data['Buy'], label="Buy", marker = "^", color= "green", lw= 3)
+plt.scatter(data.index, data['Sell'], label="Sell", marker = "v", color= "red", lw= 3 )
+plt.legend(loc="upper left")
+plt.show()
